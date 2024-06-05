6ae915e56bdb26f0cd0c1e70b48ef32b997eab42
bmorlo <benedikt.morlock.2@gmail.com>
2024-06-05 20:41:01 +0200

    one bug fix

--- pyvrp/cpp/Solution.cpp
+++ pyvrp/cpp/Solution.cpp
@@ -44,10 +44,10 @@ void Solution::evaluate(ProblemData const &data)
         // @bmorlo
         // Stores the maximum underutilization found in one of the routes.
         // We keep the name consistent with the 'Route' object even though here (for the 'Solution' object) it should be maxUnderutilization...
-        if (std::max(data.vehicleType(route.vehicleType()).capacity - static_cast<Load>(2) - route.delivery(), 0) >= underUtilization_)
+        if (std::max<Load>(data.vehicleType(route.vehicleType()).capacity - static_cast<Load>(2) - route.delivery(), 0) >= underUtilization_)
         {
             // Update the maximum underutilization of the solution.
-            underUtilization_ = std::max(data.vehicleType(route.vehicleType()).capacity - static_cast<Load>(2) - route.delivery(), 0);
+            underUtilization_ = std::max<Load>(data.vehicleType(route.vehicleType()).capacity - static_cast<Load>(2) - route.delivery(), 0);
             // Also, update the minimum load found in one of the solution's routes.
             //minLoad_ = route.delivery();
         }
